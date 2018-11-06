4946507b5a8ece20168439b8263bc7d957615aaf
jarberg <mjarberg88@gmail.com>
2018-11-06 15:41:45 +0100

    added method for nameinput

--- src/DTU/SWT_grp16/UI/Keyboard.java
+++ src/DTU/SWT_grp16/UI/Keyboard.java
@@ -1,4 +1,29 @@
 package DTU.SWT_grp16.UI;
 
+import DTU.SWT_grp16.Controller.GameController;
+
+import java.util.Scanner;
+
 public class Keyboard {
+    Scanner input;
+
+    public Keyboard(){
+
+        input = new Scanner(System.in);
+    }
+
+    public String askPlayerName(int playerNumber){
+        String playerName = "";
+
+        while(waitForInput()){
+            playerName = input.nextLine();
+            break;
+        }
+        return playerName;
+    }
+
+    public boolean waitForInput(){
+        boolean doNext = input.hasNext();
+        return doNext;
+    }
 }
