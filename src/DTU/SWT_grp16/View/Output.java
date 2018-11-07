5c25ca6866b489fc085c115a5c4a74529d7a4715
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2018-11-07 13:40:03 +0100

    Hotfix! Merge conflict created tiny issue.

--- src/DTU/SWT_grp16/View/Output.java
+++ src/DTU/SWT_grp16/View/Output.java
@@ -1,4 +1,15 @@
 package DTU.SWT_grp16.View;
 
+import DTU.SWT_grp16.UI.StringCollection;
+
 public class Output {
+
+    public Output(){
+    }
+
+    public void printFieldMessage(int fieldID){
+        String fieldMessage = StringCollection.getFieldMessageByID(fieldID);
+        System.out.println(fieldMessage);
+    }
+
 }
