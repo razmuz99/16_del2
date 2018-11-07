588aad9cf231b78aaee460fbe158ce03e46024a0
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2018-11-07 09:58:09 +0100

    Introducing StringCollection, preparing to read from files

--- src/DTU/SWT_grp16/UI/Output.java
+++ src/DTU/SWT_grp16/UI/Output.java
@@ -1,4 +1,15 @@
 package DTU.SWT_grp16.UI;
 
+import DTU.SWT_grp16.Model.Field;
+
 public class Output {
+
+    public Output(){
+    }
+
+    public void fieldMessage(int fieldID){
+        String fieldMessage = StringCollection.getFieldMessageByID(fieldID);
+        System.out.println(fieldMessage);
+    }
+
 }
---
+++ src/DTU/SWT_grp16/UI/StringCollection.java
@@ -0,0 +1,9 @@
+package DTU.SWT_grp16.UI;
+
+public class StringCollection {
+
+    static String getFieldMessageByID(int ID){
+        return "temp";
+    }
+
+}
