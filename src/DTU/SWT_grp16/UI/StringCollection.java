51239d25cc8d2ae20f0e31a7e951dcdbc1d47e04
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2018-11-07 12:08:43 +0100

    Testing BufferedReader

--- src/DTU/SWT_grp16/UI/StringCollection.java
+++ src/DTU/SWT_grp16/UI/StringCollection.java
@@ -1,9 +1,27 @@
 package DTU.SWT_grp16.UI;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class StringCollection {
 
+    //Todo: refactor methods to avoid duplication.
+
+    private static StringBuilder stringBuilder;
+    private static String FIELD_TEXT_PATH = "test.txt";
+
     static String getFieldMessageByID(int ID){
-        return "temp";
+        stringBuilder = new StringBuilder();
+        try(BufferedReader reader = new BufferedReader(new FileReader(FIELD_TEXT_PATH))){
+            while(reader.ready()){
+                String lineRead = reader.readLine();
+                if(lineRead.startsWith("id:"+ID))
+                    stringBuilder.append(lineRead);
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return stringBuilder.toString();
     }
 
 }
