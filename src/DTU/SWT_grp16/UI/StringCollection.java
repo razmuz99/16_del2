faaf820112bb91774a63cee9772d11881f30139f
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2018-11-07 12:22:05 +0100

    Working StringCollection getFieldMessageByID()

--- src/DTU/SWT_grp16/UI/StringCollection.java
+++ src/DTU/SWT_grp16/UI/StringCollection.java
@@ -8,15 +8,16 @@ public class StringCollection {
     //Todo: refactor methods to avoid duplication.
 
     private static StringBuilder stringBuilder;
-    private static String FIELD_TEXT_PATH = "test.txt";
+    private static String FIELD_TEXT_PATH = "fieldMessages.txt";
 
-    static String getFieldMessageByID(int ID){
+    public static String getFieldMessageByID(int ID){
         stringBuilder = new StringBuilder();
         try(BufferedReader reader = new BufferedReader(new FileReader(FIELD_TEXT_PATH))){
             while(reader.ready()){
                 String lineRead = reader.readLine();
-                if(lineRead.startsWith("id:"+ID))
-                    stringBuilder.append(lineRead);
+                if(lineRead.startsWith("id:"+ID)){
+                    stringBuilder.append(lineRead.substring(5));
+                }
             }
         }catch(Exception e){
             System.out.println(e.getMessage());
